b97ed54382ff5c77e4aed3c9ee4b43ea7c34373f
vamsimaru <88936655+vamsimaru@users.noreply.github.com>
2022-01-24 03:10:42 +0100

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+    type = string
+    default = "<enter your access key>"
+  
+}
+variable "secret_key" {
+    type = string
+    default = "<enter your aws secret key>"
+  
+}
+variable "key_name" {
+    type = string
+    default = "<enter the keyname that you haved saved in your AWS key pairs>"
+
+}
