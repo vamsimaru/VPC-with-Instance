0c0fba4ace43d5332c3c73f273a2bbf0111b7bb4
vamsimaru <88936655+vamsimaru@users.noreply.github.com>
2022-01-24 03:19:45 +0100

    Update var.tf

--- var.tf
+++ var.tf
@@ -8,6 +8,16 @@ variable "secret_key" {
     default = "<enter your aws secret key>"
   
 }
+variable "region" {
+    type = string
+    default = "<enter the region name>"
+
+}
+variable "ami" {
+    type = string
+    default = "<enter the ami of the region>"
+
+}
 variable "key_name" {
     type = string
     default = "<enter the keyname that you haved saved in your AWS key pairs>"
